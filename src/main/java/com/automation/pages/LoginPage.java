01d517492a018fc09445e8214cc635150e463d13
Elif Damla Pinar <damlapnar1@gmail.com>
2026-04-19 13:00:00 -0400

    Add BasePage abstraction and LoginPage POM

---
+++ src/main/java/com/automation/pages/LoginPage.java
@@ -0,0 +1,37 @@
+package com.automation.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class LoginPage extends BasePage {
+
+    @FindBy(id = "user-name")
+    private WebElement usernameInput;
+
+    @FindBy(id = "password")
+    private WebElement passwordInput;
+
+    @FindBy(id = "login-button")
+    private WebElement loginButton;
+
+    @FindBy(css = "[data-test='error']")
+    private WebElement errorMessage;
+
+    public void navigate(String baseUrl) {
+        driver.get(baseUrl);
+    }
+
+    public void login(String username, String password) {
+        type(usernameInput, username);
+        type(passwordInput, password);
+        click(loginButton);
+    }
+
+    public String getErrorMessage() {
+        return getText(errorMessage);
+    }
+
+    public boolean isErrorDisplayed() {
+        return isDisplayed(errorMessage);
+    }
+}
